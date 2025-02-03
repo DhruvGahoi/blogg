97275d0289c7acda9e1605291e37d788ed5e32d2
DhruvGahoi <dhruvgahoi11111@gmail.com>
2025-02-03 22:46:43 +0530

    added signup route

---
+++ backend/src/index.ts
@@ -0,0 +1,47 @@
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { Hono } from 'hono'
+import { sign } from 'hono/jwt'
+
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string
+  }
+}>()
+
+app.post('/api/v1/signup', async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl:  c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+
+  // create a new user
+  const body = await c.req.json();
+  const user = await prisma.user.create({
+    data: {
+      email: body.email,
+      password: body.password,
+    }
+  })
+
+  const token = sign({ id: user.id }, "secret")
+
+  return c.json({ token });
+})
+
+app.post('/api/v1/signin', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.post('/api/v1/blog', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.put('/api/v1/blog', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.get('/api/v1/blog/:id', (c) => {
+  return c.text('Hello Hono!')
+})
+
+export default app
